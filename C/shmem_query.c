65c4967e06adb49c604329a9150f3cdf1156c4e7
Siddhartha Jana <siddharthajana24@gmail.com>
2015-02-26 15:49:11 -0600

    Adding C and Fortran examples for query-based routines in spec ver.1.2     --> shmem_info_get_name and shmem_info_get_version

---
+++ C/shmem_query.c
@@ -0,0 +1,93 @@
+/*
+ *
+ * Copyright (c) 2011 - 2015
+ *   University of Houston System and Oak Ridge National Laboratory.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * o Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * 
+ * o Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * 
+ * o Neither the name of the University of Houston System, Oak Ridge
+ *   National Laboratory nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+
+/*
+ * expected output on every PE:
+ *
+ * On PE<pe-id>:
+ * Major version = <..>(SUCCESS)
+ * Minor Version = <..>(SUCCESS)
+ * Name="<..>"(SUCCESS)
+ *
+ */
+
+#include <stdio.h>
+#include <shmem.h>
+#include <string.h>
+
+#define SUCCESS "SUCCESS"
+#define FAIL "FAIL"
+#define UNDEF "UNDEFINED"
+
+int
+main ()
+{
+  char *major_status, *minor_status, *name_status, name[_SHMEM_MAX_NAME_LEN];
+  int major_ver, minor_ver;
+  int me;
+
+  shmem_init ();
+  me = shmem_my_pe ();
+  
+  shmem_info_get_version(&major_ver, &minor_ver);
+  shmem_info_get_name(name);
+
+  if ( major_ver == _SHMEM_MAJOR_VERSION )
+	  major_status = SUCCESS;
+  else 
+	  major_status = FAIL;
+
+  if ( minor_ver == _SHMEM_MINOR_VERSION )
+	  minor_status = SUCCESS;
+  else 
+	  minor_status = FAIL;
+
+  if ( strlen(name) < _SHMEM_MAX_NAME_LEN )
+	  name_status = SUCCESS;
+  else
+	  name_status = UNDEF;
+
+
+  printf ("On PE%d:\n Major version = %d(%s)\n Minor Version = %d(%s)\n Name=\"%s\"(%s)\n", me,
+		 major_ver, major_status, 
+		 minor_ver, minor_status, 
+		 name, name_status);
+
+  return 0;
+}
