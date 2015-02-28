68e2d93e4fae8b1ca4ecdc410b01102224d0f047
Tony Curtis <tonyc@uh.edu>
2015-02-28 14:19:03 -0600

    Indentation fix

--- C/global_exit.c
+++ C/global_exit.c
@@ -37,29 +37,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <shmem.h> 
+#include <shmem.h>
 
 int
-main(void)
+main (void)
 {
-  int me, npes; 
+  int me, npes;
 
-  shmem_init();
+  shmem_init ();
 
-  me = shmem_my_pe();
-  npes = shmem_n_pes();
+  me = shmem_my_pe ();
+  npes = shmem_n_pes ();
 
-  if (me == 0) {
-    FILE *fp = fopen("input.txt", "r"); 
+  if (me == 0)
+    {
+      FILE *fp = fopen ("input.txt", "r");
 
-    if (fp == NULL) {  /* Input file required by program is not available */
-      shmem_global_exit (99); /* use easy to spot exit status */
+      if (fp == NULL)
+	{			/* Input file required by program is not available */
+	  shmem_global_exit (99);	/* use easy to spot exit status */
+	}
+
+      /* do something with the file */
+
+      fclose (fp);
     }
-
-    /* do something with the file */
-
-    fclose(fp);
-  }
 
   return 0;
 }
